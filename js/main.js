ab5276e986065d07f52957073439e58aaccfe156
spursster <spursster@gmail.com>
2026-05-27 18:51:38 +0300

    Update and rename main.js to js/main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
-// js/main.js
 import { eventBus } from './core/eventBus.js';
 import { gameState } from './core/gameState.js';
 import { saveManager } from './core/saveManager.js';
 import { dataLoader } from './core/dataLoader.js';
 import { initCombatSystem } from './systems/combat.js';
-import { initUITriggers } from './ui/renderer.js';
+import { initUI } from './ui/renderer.js';
+import { initTravelSystem } from './systems/travel.js';  // ще се добави
 
-async function initGame() {
+async function bootstrap() {
     await dataLoader.loadAll();
+    initUI();
     initCombatSystem();
-    initUITriggers();
-    saveManager.load('auto');  // опит за зареждане
+    // initTravelSystem();
+    saveManager.load('auto');
     eventBus.emit('game:ready');
+    console.log("Pirate Boss е жив!");
 }
 
-initGame();
+bootstrap();
