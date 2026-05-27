a5bcb4b0287dfde94b2a274ef795ebf04813f8fd
spursster <spursster@gmail.com>
2026-05-27 19:11:49 +0300

    Update and rename eventBus.js to js/core/eventBus.js

--- js/core/eventBus.js
+++ js/core/eventBus.js
@@ -1,23 +1,19 @@
-// js/core/eventBus.js
-class EventBus {
-    constructor() {
-        this.events = new Map();
-    }
+export const eventBus = {
+    events: new Map(),
     on(event, callback) {
         if (!this.events.has(event)) this.events.set(event, []);
         this.events.get(event).push(callback);
-    }
+    },
     off(event, callback) {
         if (!this.events.has(event)) return;
         const callbacks = this.events.get(event);
         const index = callbacks.indexOf(callback);
         if (index !== -1) callbacks.splice(index, 1);
-    }
+    },
     emit(event, data) {
         if (!this.events.has(event)) return;
         this.events.get(event).forEach(cb => {
             try { cb(data); } catch(e) { console.error(e); }
         });
     }
-}
-export const eventBus = new EventBus();
+};
